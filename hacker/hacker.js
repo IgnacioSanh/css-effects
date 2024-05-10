8809119477ff6e96d11df7c3d77d0bbb6104ddb1
Ignacio Sanhueza <ignacio.sanhueza.l@gmail.com>
2024-05-10 10:52:12 -0400

    Hacker effect

---
+++ hacker/hacker.js
@@ -0,0 +1,21 @@
+const letters = "abcdefghijklmnopqrstuvwxyz";
+
+document.querySelector("h1").onmouseover = (event) => {
+	let iteration = 0;
+	const interval = setInterval(() => {
+		event.target.innerText = event.target.innerText
+			.split("")
+			.map((_, index) => {
+				if (index < iteration) {
+					return event.target.dataset.value[index];
+				}
+				return letters[Math.floor(Math.random() * 26)];
+			})
+			.join("");
+
+		if (iteration >= event.target.dataset.value.length)
+			clearInterval(interval);
+
+		iteration += 1 / 3;
+	}, 30);
+};
